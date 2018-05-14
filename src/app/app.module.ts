8de6054638bcd8825e9b479cd02a566cd02f6c16
ferdausi <asifa.cse12@gmail.com>
2018-05-15 02:34:13 +0600

    Firebase demo Project with Angular5

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import {environment} from '../environments/environment';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {AngularFireModule} from 'angularfire2';
+
+import { AppComponent } from './app.component';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
